53453a79281a974fb9ee20cee5899ca13fb1becb
Syruphie <ahmadfakhry1999@gmail.com>
2025-05-20 12:08:51 -0600

    Lab 1 Update
    Finished Lab 1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    alert('Lab 1 Done');
+  }
+
+  const groupMembers = [
+    { name: "Joy WOng", github_repo: "joywong1228", group_no: "3" },
+    { name: "Sheeba Hussaini", github_repo: "s-hussaini", group_no: "3" },
+    { name: "Ahmad Fakhry", github_repo: "syruphie", group_no: "3" }
+  ];
+
   return (
     <View style={styles.container}>
+      {groupMembers.map((member, index) => (
+        <Text key={index}>{member.name}</Text>
+      ))}
+
+      <Pressable onPress={handlePress} >
+        <Text style= {styles.button}>Press Me</Text>
+      </Pressable>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+
+  return (
+    <View  style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Pressable onPress={handlePress} >
+        <Text style= {styles.button}>Press Me</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +46,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+    backgroundColor: 'lightblue',
+    borderWidth: 2,
+    borderColor: 'black',
+    padding: 10,
+    borderRadius: 5
+  }
 });
